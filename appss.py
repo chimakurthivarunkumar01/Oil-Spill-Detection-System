8468ac69aff2d29a2c1faadbdedb64ae339fb8df
chimakurthivarunkumar01 <119066645+chimakurthivarunkumar01@users.noreply.github.com>
2026-09-21 23:34:56 +0530

    Add files via upload

---
+++ appss.py
@@ -0,0 +1,48 @@
+
+
+
+print("Helo")
+
+
+
+
+name = 'varun'  
+print(name)
+
+location = 'hyderabad'
+print(location)
+
+a,b = 10,40
+i,j = 40,40
+print(i,b)
+
+
+
+x = 70
+y = 80
+
+c = y
+y = x
+x = c
+print(x,y)
+
+# name  = input("enter your name ")
+# print(name)
+# location = input("enter your location")
+# print(location)
+
+
+acc_NUM = input("Enter your number"+": ")
+acc_name = input("Enter your name: ")
+acc_bank = input("Enter your bank name: ")
+acc_branch = input("Enter your branch name: ")
+
+
+print(acc_NUM)
+print(acc_name)
+print(acc_bank)
+print(acc_branch)
+
+####################################
+
+
